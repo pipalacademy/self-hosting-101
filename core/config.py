6ff4918bb9b55c4efb09e4587f0e8d2734a80b27
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-10-19 13:04:07 +0530

    use uppercase for env vars

--- core/config.py
+++ core/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-_app_root = os.environ.get("app_root")
+_app_root = os.environ.get("APP_ROOT")
 
 db_path = f"{_app_root}/private/treadmill.db" if _app_root else "treadmill.db"
 db_uri = f"sqlite:///{db_path}"
